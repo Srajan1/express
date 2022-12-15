0665496a44f7af76e67d91a63a783b60ec170e57
srajan <srakishor.g@gmail.com>
2022-12-15 22:38:51 +0530

    connecting to pg

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express();
 
-app.use(express.json());
-
-app.use(require('./server/routes/student'))
-app.use(require('./server/routes/subject'))
+require('./server/setupEnv')(app, express);
 
 app.get('/', (req, res) =>{
-    res.json({msg: "Hello world"})
+    res.send("Hello world")
 })
 
 app.listen('3000', () => {
